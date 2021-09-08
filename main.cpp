31068fca1b598bbca33254af940e89c5df3aacbb
Rupesh Yadav <86247908+rupesh0097@users.noreply.github.com>
2021-09-08 01:20:05 +0000

    I added another program to check if the given number is even, odd or  zero.

--- main.cpp
+++ main.cpp
@@ -4,20 +4,47 @@ using namespace std;
 int main(){
     //C++ Conditions
     //Write a programe to check if the given number is positve, negative or zero.
-    int number;
+    int num;
     cout << "Enter the number: ";
-    cin >> number;
+    cin >> num;
     
-    if (number>0){
-        cout << "The number is Positive.";
+    if (num>0){
+        cout << "The number is Positive."<< endl;
     }
-    else if(number<0){
-        cout << "The given number is negative";
+    else if(num<0){
+        cout << "The given number is negative" << endl;
     }
     else{
-        cout << "The number is zero.";
-    }
-    return 0;
+        cout << "The number is zero."<< endl << endl;
+    }   
+
+
+		//C++ Nested if Condition.
+    //If statement inside another if statement is nested if condition.
+    //Write a programe to check if the given number is even, odd or neither.
+        
+         int number;
+         cout << "The given number is: ";
+         cin >> number;
+         
+         if (number != 0){
+                if ((number % 2) == 0 ){
+                    cout << "The given number is even." << endl;
+                }
+                else{
+                    cout << "The given number is odd." << endl;
+                }
+         }
+        else{
+            cout << "The given number is neithe even nor odd instead it is zero." << endl;
+            cout << "This is always printed.";
+        }
+
+		
+    
+		
+		
+		return 0;
     
     
 }
